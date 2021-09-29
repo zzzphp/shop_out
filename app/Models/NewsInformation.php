b8df56eb33e693731c300b1153ed05978dfbd9c1
hz <491315686@qq.com>
2021-09-29 11:32:38 +0800

    优化

--- app/Models/NewsInformation.php
+++ app/Models/NewsInformation.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Dcat\Admin\Traits\HasDateTimeFormatter;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class NewsInformation extends Model
 {
@@ -14,4 +16,14 @@ class NewsInformation extends Model
         'title','summary', 'content', 'resource', 'resource_url',
         'resource_id','author', 'thumbnail','created_at'
     ];
+
+    protected $appends = ['thumbnail_url'];
+
+    public function getThumbnailUrlAttribute()
+    {
+        if (Str::startsWith($this->attributes['thumbnail'], ['http://', 'https://'])) {
+            return $this->attributes['thumbnail'];
+        }
+        return Storage::disk('admin')->url($this->attributes['thumbnail']);
+    }
 }
