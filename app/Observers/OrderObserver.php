e04b55f043a9c20217ac1e2d27a6dcf06b6ffacd
hz <491315686@qq.com>
2021-11-05 16:03:44 +0800

    通知

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -37,7 +37,10 @@ class OrderObserver
             noticeHelper(['phone' => [$order->user->phone], 'data' => [$order->product->title, '锁单']]);
         }
         if ($order->status === Order::STATUS_SUCCESS) {
-            noticeHelper(['phone' => [$order->user->phone], 'data' => [$order->product->title, '支付成功']]);
+            noticeHelper(['phone' => [$order->user->phone], 'data' => [$order->no, '支付成功']]);
+        }
+        if ($order->status === Order::STATUS_RECEIVING) {
+            noticeHelper(['phone' => [$order->user->phone], 'data' => [$order->no, '成功发货']]);
         }
     }
 
